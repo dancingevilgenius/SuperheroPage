80cc2c552f699bd5cd2549895e164e8320e47484
Carlos Garcia <dancingevilgenius@gmail.com>
2026-02-28 01:40:17 -0600

    First checkin. Copy of a CodeWithMosh tutorial. Simple display list of superheroes.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import ListGroup from "./components/ListGroup/ListGroup";
+import UserList from "./components/UserList";
+
+function App() {
+  //return <UserList />;
+  return (
+    <ListGroup
+      items={["Superman", "Batman", "Wonderwoman", "Green Lantern"]}
+      heading={"Heroes"}
+      onSelectItem={function (item: string): void {
+        throw new Error("Function not implemented.");
+      }}
+    />
+  );
+}
+
+export default App;
