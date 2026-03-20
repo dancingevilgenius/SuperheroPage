a7c895b2540481781871d977b7b3a9316e998dd9
Carlos Garcia <dancingevilgenius@gmail.com>
2026-03-20 05:06:57 -0500

    Dropdown menu moved to top level. Merged 'Submit' and 'Order by' in dropdown.

--- src/components/ContestantNavbar.tsx
+++ src/components/ContestantNavbar.tsx
@@ -9,7 +9,28 @@ function ContestantNavBar() {
     <Navbar expand="lg" bg="dark" data-bs-theme="dark">
       <Container>
         <Navbar.Brand href="#home">Nov Country Swing</Navbar.Brand>
-        <Button variant="outline-light">Submit</Button>
+
+        <NavDropdown
+          title="Actions"
+          id="nav-dropdown"
+          style={{ color: "white" }}
+        >
+          <NavDropdown.Item eventKey="1.1">Submit Scores</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Header>Order By</NavDropdown.Header>
+          <NavDropdown.Item eventKey="4.1">Bib #</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.2">Raw Score</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.3">Not Scored at top</NavDropdown.Item>
+
+          <NavDropdown.Item eventKey="4.5">Leader First Name</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.6">Leader Last Name</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.7">
+            Follower First Name
+          </NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.8">Follower Last Name</NavDropdown.Item>
+        </NavDropdown>
+
+        {/* <Button variant="outline-light">Submit </Button> */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" style={{ margin: "10px auto" }}>
           <Nav className="me-auto">
@@ -22,24 +43,6 @@ function ContestantNavBar() {
 
             <Nav.Link href="#link">Link</Nav.Link>
           </Nav>
-          <NavDropdown title="Order by" id="nav-dropdown" menuVariant="light">
-            <NavDropdown.Item eventKey="4.1">Bib #</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.2">Raw Score</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.3">
-              Not Scored at top
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item eventKey="4.5">
-              Leader First Name
-            </NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.6">Leader Last Name</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.7">
-              Follower First Name
-            </NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.8">
-              Follower Last Name
-            </NavDropdown.Item>
-          </NavDropdown>
         </Navbar.Collapse>
       </Container>
     </Navbar>
