81d4f8f0e7492474849ffa27a9e25be96a95ec00
Carlos Garcia <dancingevilgenius@gmail.com>
2026-03-20 08:15:04 -0500

    Surrounded bib numbers inside a gray Badge tags.

--- src/components/ContestantList.tsx
+++ src/components/ContestantList.tsx
@@ -1,4 +1,5 @@
 import Accordion from "react-bootstrap/Accordion";
+import Badge from "react-bootstrap/Badge";
 import RawScoreTripleDigit from "./RawScoreTripleDigit";
 import RawScoreDigit from "./RawScoreDigit";
 import ContestantDetails from "./ContestantDetails";
@@ -11,37 +12,67 @@ function ContestantList() {
     <>
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
-          <Accordion.Header>#101 Clark Kent and Lois Lane</Accordion.Header>
+          <Accordion.Header>
+            <Badge bg="secondary" style={{ marginRight: "5px" }}>
+              #101
+            </Badge>{" "}
+            Clark Kent and Lois Lane
+          </Accordion.Header>
           <Accordion.Body>
             <ContestantDetails />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>#102 Bruce Wayne and Selina Kyle</Accordion.Header>
+          <Accordion.Header>
+            <Badge bg="secondary" style={{ marginRight: "5px" }}>
+              #102
+            </Badge>{" "}
+            Bruce Wayne and Selina Kyle
+          </Accordion.Header>
           <Accordion.Body>
             <ContestantDetails />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
-          <Accordion.Header>#103 Oliver McQeen and Dina Lance</Accordion.Header>
+          <Accordion.Header>
+            <Badge bg="secondary" style={{ marginRight: "5px" }}>
+              #103
+            </Badge>{" "}
+            Oliver McQeen and Dina Lance
+          </Accordion.Header>
           <Accordion.Body>
             <ContestantDetails />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="3">
-          <Accordion.Header>#104 Hal Jordan and Carol Ferris</Accordion.Header>
+          <Accordion.Header>
+            <Badge bg="secondary" style={{ marginRight: "5px" }}>
+              #104
+            </Badge>{" "}
+            Hal Jordan and Carol Ferris
+          </Accordion.Header>
           <Accordion.Body>
             <ContestantDetails />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="4">
-          <Accordion.Header>#105 John Jones and Diana Prince</Accordion.Header>
+          <Accordion.Header>
+            <Badge bg="secondary" style={{ marginRight: "5px" }}>
+              #105
+            </Badge>{" "}
+            John Jones and Diana Prince
+          </Accordion.Header>
           <Accordion.Body>
             <ContestantDetails />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="5">
-          <Accordion.Header>#106 Arthur Curry and Mera Curry</Accordion.Header>
+          <Accordion.Header>
+            <Badge bg="secondary" style={{ marginRight: "5px" }}>
+              #106
+            </Badge>{" "}
+            Arthur Curry and Mera Curry
+          </Accordion.Header>
           <Accordion.Body>
             <ContestantDetails />
           </Accordion.Body>
